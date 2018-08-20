bc56ad3d26a043eb2651a78304f8c380c826ce94
Josh <josh@fortestrong.com.au>
2018-08-20 21:37:45 +1000

    Update for RS encryption algorithm

--- src/index.js
+++ src/index.js
@@ -1,45 +1,51 @@
-import jwt from 'express-jwt';
-import ArgumentError from './errors/ArgumentError';
+import jwt from "express-jwt";
+const jwksRsa = require("jwks-rsa");
+import ArgumentError from "./errors/ArgumentError";
 
-module.exports = (options) => {
-  if (!options || !(options instanceof Object)) {
-    throw new ArgumentError('The options must be an object.');
-  }
+module.exports = options => {
+    if (!options || !(options instanceof Object)) {
+        throw new ArgumentError("The options must be an object.");
+    }
 
-  if (!options.clientId || options.clientId.length === 0) {
-    throw new ArgumentError('The Auth0 Client ID has to be provided.');
-  }
+    if (!options.clientId || options.clientId.length === 0) {
+        throw new ArgumentError("The Auth0 Client ID has to be provided.");
+    }
 
-  if (!options.clientSecret || options.clientSecret.length === 0) {
-    throw new ArgumentError('The Auth0 Client Secret has to be provided.');
-  }
+    if (!options.clientSecret || options.clientSecret.length === 0) {
+        throw new ArgumentError("The Auth0 Client Secret has to be provided.");
+    }
 
-  if (!options.domain || options.domain.length === 0) {
-    throw new ArgumentError('The Auth0 Domain has to be provided.');
-  }
+    if (!options.domain || options.domain.length === 0) {
+        throw new ArgumentError("The Auth0 Domain has to be provided.");
+    }
 
-  const middleware = jwt({
-    secret: new Buffer(options.clientSecret, 'base64'),
-    audience: options.clientId,
-    issuer: 'https://' + options.domain + '/'
-  });
+    const middleware = jwt({
+        secret: jwksRsa.expressJwtSecret({
+            cache: true,
+            rateLimit: true,
+            jwksRequestsPerMinute: 5,
+            jwksUri: `https://maddocks.au.auth0.com/.well-known/jwks.json`
+        }),
+        audience: options.clientId,
+        issuer: "https://" + options.domain + "/"
+    });
 
-  return (next) => {
-    return (context, req) => {
-      middleware(req, null, (err) => {
-        if (err) {
-          context.res = {
-            status: err.status || 500,
-            body: {
-              message: err.message
-            }
-          };
+    return next => {
+        return (context, req) => {
+            middleware(req, null, err => {
+                if (err) {
+                    context.res = {
+                        status: err.status || 500,
+                        body: {
+                            message: err.message
+                        }
+                    };
 
-          return context.done();
-        }
+                    return context.done();
+                }
 
-        return next(context, req);
-      });
+                return next(context, req);
+            });
+        };
     };
-  };
 };
